b8aa4a0e34d1056890eb9722cfc0068684fae1a1
Sensey <igorreli@ukr.net>
2021-09-17 17:00:14 +0300

    first commit

---
+++ components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    Banner
+  </div>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Vue from 'vue';
+
+    interface User {
+        firstName: string,
+        lastName: string
+    }
+
+export default Vue.extend({
+  props: {
+    data: {
+      type: Object as () => User,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      num: 11,
+    };
+  },
+  computed: {
+    counter(): number {
+      return this.num as number;
+    },
+  },
+  mounted(): void {
+    console.log(this.data);
+    this.getUser({ firstName: 'ada', lastName: 'ddd' });
+  },
+  methods: {
+    getUser({ firstName, lastName }: User): void {
+      console.log(firstName, lastName);
+    },
+  },
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    home page
+    <Banner :data="{ firstName: 24, lastName: 214}" />
+  </div>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Vue from 'vue';
+import Banner from '~/components/Banner.vue';
+
+export default Vue.extend({
+  components: {
+    Banner,
+  },
+});
+</script>
